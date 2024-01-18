80d0056718d2eab1ede2bda8254e7adc81444839
Huseynbayramli <huseynbayramli002@gmail.com>
2024-01-18 23:13:06 +0400

    qrcode

---
+++ app.js
@@ -0,0 +1,18 @@
+let input = document.querySelector('input')
+let btn = document.querySelector('button')
+let img = document.querySelector('img')
+let err = document.querySelector('.err')
+// input.addEventListener('keyup', () => {
+
+// })
+
+btn.addEventListener('click', () => {
+    let qrvalue = input.value;
+    if (qrvalue == "") {
+        err.innerText = 'Nəsə səhvlik var :(';
+    }
+    else {
+        err.innerText=''
+        img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrvalue}`
+    }
+})
